59ae910b768cf71762a4ae70ad1ba517c28cefd5
Eddie Gallaty <50750835+Eddie-Gallaty@users.noreply.github.com>
2019-05-23 16:15:34 -0400

    Add files via upload

---
+++ Install-File_Remote.ps1
@@ -0,0 +1,22 @@
+#This script is intended to solve the double hop issue with powershell
+#It copies files from local to remote then executes.
+
+#csv for hostnames
+$clients  = import-csv C:\Users\User\Scripts\Powershell\Hostnames_test.csv
+
+#build hostname "$client" object to be passed via invoke-command
+
+foreach($pc in $clients)
+{
+   $client = $pc.Name
+
+   #Copy bat file and exe from local to server side
+
+   Copy-Item –Path C:\Users\User\Scripts\Powershell\HelloWorld_Install.bat –Destination \\$client\c$\temp\
+   Copy-Item –Path C:\Users\User\Scripts\Powershell\HelloWorld.exe –Destination \\$client\c$\temp\
+
+   #invoke serverside command to install batch file.
+   Invoke-Command -ComputerName $client -ScriptBlock {Invoke-Expression -Command:"cmd.exe /c 'C:\\temp\HelloWorld.bat'" }
+}
+
+#Done like chicken dinner.
---
+++ Uninstall-File_Remote.ps1
@@ -0,0 +1,2 @@
+#this will loop thru a text file for hostnames and uninstall a program called "HelloWorld"
+Invoke-Command -Credential bh\ -ComputerName (Get-Content C:\users\sr182801\documents\scripts\Powershell\computernames.txt) -ScriptBlock {Get-CimInstance -ClassName Win32_Product -Filter "name like '%HelloWorldt%'" | Invoke-CimMethod -MethodName Uninstall}
